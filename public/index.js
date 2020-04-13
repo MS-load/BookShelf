8311a9a2dfc205817856a72135e9cb19cb1c3c36
mamta srinivas <mamtasrinivassp@gmail.com>
2020-04-13 15:50:29 +0200

    cors issue

--- public/index.js
+++ public/index.js
@@ -1,11 +1,54 @@
 fetch('http://localhost:3000/api/books/')
-.then(response => response.json())
-.then(data => {
-  console.log(data)
-})
-.catch(error => console.error(error))
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    printBooks(data)
+  })
+  .catch(error => console.error(error))
 
+function printBooks(data) {
+  data.forEach(element => {
+    const row = document.querySelector('.row')
+    const col = document.createElement('div')
+    col.classList.add("column")
+    const card = document.createElement('div')
+    card.classList.add("card")
+    card.setAttribute("data-id", element.id)
+    const title = document.createElement('h4')
+    const text = document.createElement('p')
+    const del = document.createElement('button')
+    del.innerHTML = `Delete`
+    del.onclick = deleteBook
+    title.innerHTML = element.name
+    text.innerHTML = `author: ${element.author} <br/> year: ${element.year}`
+    card.appendChild(title)
+    card.appendChild(text)
+    card.appendChild(del)
+    col.appendChild(card)
+    row.appendChild(col)
+  });
+}
 
+class RequestDelete {
+  async  delete ( url ) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+    } );
+		return  "Data slime operation successful" ;
+		// We printed it ourselves as there will be a blank object that turns to us because it is deleted too.
+  }
+}
+
+function deleteBook() {
+  const id = event.target.parentNode.getAttribute('data-id')
+  // const req= new RequestDelete();
+  // req.delete(`http://localhost:3000/book/${id}`)
+  // .then(message => console.log(message))
+  // .catch(err => console.log(err));
+  // Reloading the page
+  //location.reload();
+
+}
 
 // getDevices = async () => {
 //     const location = window.location.hostname;
